2f9555bac0dfb16ecce3ea521598a42db71a44be
Dmitriy Fomin <79067773397@yandex.ru>
2020-11-26 12:45:40 +0300

    new buttons actions

--- battleship+/client/src/ru/fomin/battleship/client/gui/PreparingForGameFrame.java
+++ battleship+/client/src/ru/fomin/battleship/client/gui/PreparingForGameFrame.java
@@ -9,8 +9,19 @@ import ru.fomin.network.SocketThread;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
-public class PreparingForGameFrame extends JFrame {
+public class PreparingForGameFrame extends JFrame implements ActionListener {
+    private final String INFO_ABOUT_GAME="<html>1)Select the \"REMOVE\" or \"POST\" mode\"<br>2)Click on the field and click \"POST\" to place the ship" +
+            "<br>3)Click on the ship for removing in the \"REMOVE\" mode"+
+            "<br>4)If you want to cancel the selected cells, click to \"CANCEL ACTION\"<br>5)For exit to registration menu click to \"EXIT\"" +
+            "<br>6)If you have saved maps, you can download the map by clicking \"LOAD THE MAP\"<br>7)You can only start the game by placing all the ships. To start the game, click \"START\"" +
+            "<br>8)There should be an empty space around each ship<br>9)The game has 4 single-deck, 3 double-deck, 2 three-deck and 1 four-deck ships</html>";
+    private final String INFO_ABOUT_DEVELOPER="<html>Developer: Dmitriy Fomin<br>GitHub: https://github.com/FominDV <br> Email: 79067773397@yandex.ru</html>";
+    private final String START="START";
+    private final String STOP="<html>STOP SEARCHING<br>OPPONENT</html>";
     private final int SIZE_OF_MAP=10;
     private final int WIDTH = 700;
     private final int HEIGHT = 600;
@@ -22,20 +33,24 @@ public class PreparingForGameFrame extends JFrame {
     private SearchOpponentThread searchOpponentThread=null;
     private MapBuilder mapBuilder;
     private final Color COLOR_OF_BACKGROUND=new Color(99, 234, 234);
+    private Color COLOR_OF_START=new Color(215, 99, 99);
 
     private final Font FONT_FOR_BUTTONS=new Font(Font.SERIF, Font.BOLD, 16);
     
     private final JPanel PANEL_MAP=new JPanel(new GridLayout(SIZE_OF_MAP, SIZE_OF_MAP));
     private final JPanel WRAPPER_FOR_MAP=new JPanel(new GridBagLayout());
     private final JPanel PANEL_BOTTOM=new JPanel(new GridLayout(2,3));
+    private final JPanel PANEL_TOP=new JPanel(new GridLayout(1,2));
 
     private final JButton BUTTON_POST=new JButton("POST");
     private final JButton BUTTON_REMOVE=new JButton("REMOVE");
     private final JButton BUTTON_CANCEL=new JButton("<html>CANCEL<br>ACTION</html>");
     private final JButton BUTTON_LOAD=new JButton("<html><p align='center'>LOAD<br>THE MAP</p></html>");
-    private final JButton BUTTON_START=new JButton("START");
+    private final JButton BUTTON_START=new JButton(START);
     private final JButton BUTTON_EXIT=new JButton("EXIT");
 
+    private final JButton BUTTON_INFO=new JButton("INSTRUCTION MANUAL");
+    private final JButton BUTTON_DEVELOPER_INFO=new JButton("DEVELOPER");
 
 
     public PreparingForGameFrame(SocketThread socketThread, String nickname, WorkingWithNetwork listener) {
@@ -64,9 +79,23 @@ public class PreparingForGameFrame extends JFrame {
         BUTTON_LOAD.setFont(FONT_FOR_BUTTONS);
         BUTTON_LOAD.setBackground(COLOR_OF_BACKGROUND);
         BUTTON_START.setFont(FONT_FOR_BUTTONS);
-        BUTTON_START.setBackground(COLOR_OF_BACKGROUND);
+        BUTTON_START.setBackground(COLOR_OF_START);
         BUTTON_EXIT.setFont(FONT_FOR_BUTTONS);
         BUTTON_EXIT.setBackground(COLOR_OF_BACKGROUND);
+        BUTTON_INFO.setFont(FONT_FOR_BUTTONS);
+        BUTTON_DEVELOPER_INFO.setFont(FONT_FOR_BUTTONS);
+
+        BUTTON_DEVELOPER_INFO.addActionListener(this);
+        BUTTON_INFO.addActionListener(this);
+        BUTTON_POST.addActionListener(this);
+        BUTTON_REMOVE.addActionListener(this);
+        BUTTON_CANCEL.addActionListener(this);
+        BUTTON_LOAD.addActionListener(this);
+        BUTTON_START.addActionListener(this);
+        BUTTON_EXIT.addActionListener(this);
+
+        PANEL_TOP.add(BUTTON_INFO);
+        PANEL_TOP.add(BUTTON_DEVELOPER_INFO);
         PANEL_BOTTOM.add(BUTTON_POST);
         PANEL_BOTTOM.add(BUTTON_REMOVE);
         PANEL_BOTTOM.add(BUTTON_CANCEL);
@@ -75,6 +104,7 @@ public class PreparingForGameFrame extends JFrame {
         PANEL_BOTTOM.add(BUTTON_EXIT);
         add(WRAPPER_FOR_MAP, BorderLayout.EAST);
         add(PANEL_BOTTOM, BorderLayout.SOUTH);
+        add(PANEL_TOP, BorderLayout.NORTH);
 
         setVisible(true);
     }
@@ -94,6 +124,7 @@ public class PreparingForGameFrame extends JFrame {
         if(searchOpponentThread==null|| !(searchOpponentThread.isAlive())) {
             listener.sendMessageToServer(LibraryOfPrefixes.MESSAGE_ABOUT_START_SEARCHING);
             searchOpponentThread = new SearchOpponentThread(this);
+            BUTTON_START.setText(STOP);
         }
             }
 
@@ -124,6 +155,30 @@ public class PreparingForGameFrame extends JFrame {
         searchOpponentThread.interrupt();
         searchOpponentThread.stop();
         listener.sendMessageToServer(LibraryOfPrefixes.STOP_SEARCHING);
+        BUTTON_START.setText(START);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+        if (source.equals(BUTTON_DEVELOPER_INFO)) {
+            showDeveloperInfo();
+            return;
+        }
+        if (source.equals(BUTTON_INFO)) {
+            showInstruction();
+            return;
+        }
+        if (source.equals(BUTTON_EXIT)) {
+            SOCKET_THREAD.close();
+            return;
+        }
     }
 
+    private void showInstruction() {
+        JOptionPane.showMessageDialog(null, INFO_ABOUT_GAME, "INSTRUCTION MANUAL", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    private void showDeveloperInfo() {
+        JOptionPane.showMessageDialog(null, INFO_ABOUT_DEVELOPER, "INFORMATION ABOUT DEVELOPER", JOptionPane.INFORMATION_MESSAGE);
+    }
 }
