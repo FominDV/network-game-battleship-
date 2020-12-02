ef5eae3aa3c9bb3c9ffe6e5e5939d0fc9f903c2e
Dmitriy Fomin <79067773397@yandex.ru>
2020-12-03 00:53:58 +0300

    add setCaretPosition

--- battleship+/client/src/ru/fomin/battleship/client/gui/OnlineGameWindow.java
+++ battleship+/client/src/ru/fomin/battleship/client/gui/OnlineGameWindow.java
@@ -273,30 +273,33 @@ public class OnlineGameWindow extends JFrame implements ActionListener {
         return pastMode;
     }
 
-    public void appendIntoLog(String message){
+    public void appendIntoLog(String message) {
         LOG.append(message);
+        LOG.setCaretPosition(LOG.getDocument().getLength());
     }
 
     public void appendIntoLog(String message, boolean isActionAgain) {
-        message=NICK_NAME + ":\n*Used mode of " + createMessageAboutMode()+"\n"+message+"\n*"+createMessageAboutNextTurn(isActionAgain)+"\n";
-      LOG.append(message);
-      listener.sendMessageToServer(LibraryOfPrefixes.getLogMessage(message));
+        message = NICK_NAME + ":\n*Used mode of " + createMessageAboutMode() + "\n" + message + "\n*" + createMessageAboutNextTurn(isActionAgain) + "\n";
+        LOG.append(message);
+        LOG.setCaretPosition(LOG.getDocument().getLength());
+        listener.sendMessageToServer(LibraryOfPrefixes.getLogMessage(message));
     }
 
-    private String createMessageAboutNextTurn(boolean isActionAgain){
-        if(isActionAgain) return "Can action again"; else return "End game turn";
+    private String createMessageAboutNextTurn(boolean isActionAgain) {
+        if (isActionAgain) return "Can action again";
+        else return "End game turn";
     }
 
     private String createMessageAboutMode() {
-        switch (pastMode){
+        switch (pastMode) {
             case 0:
-                return format("'simple shoot' (%s;%s)",pastUsingCellForActionCoordinates[0]+1,pastUsingCellForActionCoordinates[1]+1);
+                return format("'simple shoot' (%s;%s)", pastUsingCellForActionCoordinates[0] + 1, pastUsingCellForActionCoordinates[1] + 1);
             case 1:
-                return format("'volley shoot' (%s;%s)",pastUsingCellForActionCoordinates[0]+1,pastUsingCellForActionCoordinates[1]+1);
+                return format("'volley shoot' (%s;%s)", pastUsingCellForActionCoordinates[0] + 1, pastUsingCellForActionCoordinates[1] + 1);
             case 2:
-                return format("'exploration of the map' (%s;%s)",pastUsingCellForActionCoordinates[0]+1,pastUsingCellForActionCoordinates[1]+1);
+                return format("'exploration of the map' (%s;%s)", pastUsingCellForActionCoordinates[0] + 1, pastUsingCellForActionCoordinates[1] + 1);
             case 3:
-                return format("'shooting on straight' (%s;%s)",pastUsingCellForActionCoordinates[0]+1,pastUsingCellForActionCoordinates[1]+1);
+                return format("'shooting on straight' (%s;%s)", pastUsingCellForActionCoordinates[0] + 1, pastUsingCellForActionCoordinates[1] + 1);
             default:
                 return "";
         }
